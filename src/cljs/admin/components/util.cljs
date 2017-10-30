81d6e2269d3488572646575425c136ee74bb6207
Josh Lehman <jalehman37@gmail.com>
2017-10-30 10:16:58 -0700

    remove clojurescript

--- src/cljs/admin/components/util.cljs
+++
@@ -1,5 +0,0 @@
-(ns admin.components.util)
-
-(def divider
-  [:span {:dangerouslySetInnerHTML {:__html "||"}
-          :style                   {:margin-left 6 :margin-right 6}}])
--- src/cljs/starcity/dom.cljs
+++
@@ -1,9 +0,0 @@
-(ns starcity.dom
-  (:refer-clojure :exclude [val]))
-
-(defn val [event]
-  (.. event -currentTarget -value))
-
-
-(defn checked [event]
-  (.. event -currentTarget -checked))
