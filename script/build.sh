c571adb21966ad3d2bfb9c67eec710a856e281c7
Josh Lehman <jalehman37@gmail.com>
2018-02-10 13:37:45 -0800

    use single build script

--- script/build.sh
+++ script/build.sh
@@ -7,3 +7,5 @@ sass -E "UTF-8" style/sass/public.scss:resources/public/assets/css/public.css
 echo "Putting slick assets where they're supposed to go..."
 cp -r node_modules/slick-carousel/slick/fonts resources/public/assets/css
 cp node_modules/slick-carousel/slick/ajax-loader.gif resources/public/assets/css
+echo "Compiling Clojure..."
+lein uberjar
